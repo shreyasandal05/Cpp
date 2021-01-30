5df9e0438227ffbd9c44667f54dc2feb713ecfbc
Shreya Sandal <sandalshreya@gmail.com>
2021-01-30 19:44:59 +0530

    Program to reverse a given string

---
+++ recursion_reverseString.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+void reverse(string s){
+	if(s.length()==0){
+		return;
+	}
+	string restOfString=s.substr(1);
+	reverse(restOfString);
+	cout<<s[0];
+}
+
+int main(){
+	string s;
+	cin>>s;
+	reverse(s);
+	return 0;
+	
+}
